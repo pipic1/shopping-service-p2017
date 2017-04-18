013c834714894a3661debf3e5a1406dc84a7f579
Aurelien Pillevesse <Aurelien Pillevesse>
2017-04-18 21:11:38 +0200

    come back connection db

--- src/main/java/io/github/aurelienpillevesse/MyResource.java
+++ src/main/java/io/github/aurelienpillevesse/MyResource.java
@@ -1,6 +1,5 @@
 package io.github.aurelienpillevesse;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -64,12 +63,7 @@ public class MyResource {
     }
     
     private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return DriverManager.getConnection(dbUrl);
     }
 }
