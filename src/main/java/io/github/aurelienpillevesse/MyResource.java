dfa3a1e6d98110454cf08de9fc43c8cdade379d8
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2017-04-22 15:44:11 +0200

    add import

--- src/main/java/io/github/aurelienpillevesse/MyResource.java
+++ src/main/java/io/github/aurelienpillevesse/MyResource.java
@@ -18,6 +18,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.DefaultValue;
 
 /**
  * Root resource (exposed at "book" path)
