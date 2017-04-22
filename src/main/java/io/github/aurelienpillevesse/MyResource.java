e74cc67c0caba7edae24a8ff7e6d595599b59bcb
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2017-04-22 10:55:09 +0200

    reimport

--- src/main/java/io/github/aurelienpillevesse/MyResource.java
+++ src/main/java/io/github/aurelienpillevesse/MyResource.java
@@ -14,7 +14,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "book" path)
