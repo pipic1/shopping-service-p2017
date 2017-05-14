b37f5be4b8b5c158e673aa2d7791d47ed651f2f3
Aurelien Pillevesse <Aurelien Pillevesse>
2017-05-14 16:13:17 +0200

    convert book return to customresponse

--- src/main/java/io/github/aurelienpillevesse/MyResource.java
+++ src/main/java/io/github/aurelienpillevesse/MyResource.java
@@ -22,81 +22,34 @@ import io.github.aurelienpillevesse.model.CustomResponse;
  */
 @Path("book")
 public class MyResource {
-
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    /*@GET
-    @Path("book")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getBook(
-    	//@QueryParam("account") int id,
-    	@QueryParam("isbn") int isbn
-    	//@QueryParam("from") String from,
-    	//@QueryParam("to") String to,
-    	//@QueryParam("corr") int corr
-    ) {
-    	Boolean isbnExists = null;
-    	ResultSet rs;
-    	String output = "";
-    	output += "isbn = "+ isbn + "\n";
-    	try {
-			PreparedStatement st = getConnection().prepareStatement("select * from books where ISBN = ? ");
-			st.setInt(1,isbn);
-			//Statement st = getConnection().createStatement();
-			rs = st.executeQuery();
-			while (rs.next()) {
-			    output += rs.getString("isbn") + ", ";
-			    output += rs.getString("book_name") + ", ";
-			    output += rs.getString("publisher_name") + "\n";
-			}
-			rs.close();
-			st.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			output += "ici\n";
-		}
-    	return output;
-    }*/
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Book getBook(
+    public CustomResponse getBook(
     	@QueryParam("account") int id,
     	@DefaultValue("-1") @QueryParam("isbn") String isbn,
     	@QueryParam("from") String from,
     	@QueryParam("to") String to,
     	@QueryParam("corr") int corr
-        ) {
+    )
+    {
+    	CustomResponse cr = new CustomResponse();
     	DAO<Book> dao = new BookDAO();
     	Book book = null;
-      book = dao.find(isbn);
+    	book = dao.find(isbn);
 
-      if(book.getIsbn() != null) {
-          Client client = ClientBuilder.newClient();
-          WebTarget target = client.target("https://stock-service-p2017.herokuapp.com").path("bookStock");
-          Response r = target.request().post(Entity.json(book));
-          return r.readEntity(Book.class);
-      }
-
-      return null;
+    	if(book.getIsbn() == null) {
+    		cr.setData(null);
+        	cr.setData("Book unavailable");
+        	return cr;
+    	}
+    	
+    	Client client = ClientBuilder.newClient();
+    	WebTarget target = client.target("https://stock-service-p2017.herokuapp.com").path("bookStock");
+    	Response r = target.request().post(Entity.json(book));
+    	//return r.readEntity(Book.class);
+    	
+    	cr.setData(r.readEntity(Book.class));
+    	cr.setMessage("Book available");
+    	return cr;
     }
-
-    /*private static Connection getConnection() throws URISyntaxException, SQLException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        return DriverManager.getConnection(dbUrl);
-    }
-
-    private static Connection getConnection2() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
-    }*/
 }
